0a4b8abb7633ab09527882a7f1d47fcbfe158dcd
caifangpi <caifangpi@yonyou.com>
2021-03-24 22:06:39 +0800

    谷粒商城微服务基础增删改查代码提交

--- gulimall-coupon/src/main/java/com/rs/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/rs/gulimall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +31,24 @@ import com.rs.common.utils.R;
 public class CouponController {
     @Autowired
     private CouponService couponService;
+    @Value("${coupon.user.name}")
+    private String userName;
+    @Value("${coupon.user.age}")
+    private Integer age;
+    @Value("${gulimall.user.name}")
+    private String userName2;
+
+    @RequestMapping("/test")
+    public R test(){
+        return R.ok().put("name", userName).put("age", age).put("gulimall-user", userName2);
+    }
+
+    @RequestMapping("/member/list")
+    public R membercoupons(){
+        CouponEntity couponEntity = new CouponEntity();
+        couponEntity.setCouponName("满100减10");
+        return R.ok().put("coupons", Arrays.asList(couponEntity));
+    }
 
     /**
      * 列表
