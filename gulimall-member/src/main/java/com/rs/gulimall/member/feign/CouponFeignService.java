0b5a540d982e246600bbcaeec65343e74a9e4611
caifangpi <caifangpi@yonyou.com>
2021-03-24 22:06:19 +0800

    谷粒商城微服务基础增删改查代码提交

---
+++ gulimall-member/src/main/java/com/rs/gulimall/member/feign/CouponFeignService.java
@@ -0,0 +1,15 @@
+package com.rs.gulimall.member.feign;
+
+import com.rs.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Component
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+
+    @RequestMapping("/coupon/coupon/member/list")
+    public R membercoupons();
+
+}
